fa2c3a9d7782b042766d35572736bb3b1c7d5fb2
zou_gj <751608899@qq.com>
2018-12-20 15:26:53 +0800

    update proto

--- src/Cfwf/Micro_service/Resource_statistics/ActiveUserItem.php
+++ src/Cfwf/Micro_service/Resource_statistics/ActiveUserItem.php
@@ -18,9 +18,23 @@ class ActiveUserItem extends \Google\Protobuf\Internal\Message
      */
     private $date = 0;
     /**
+     *总人数
+     *
      * Generated from protobuf field <code>int64 user_count = 2;</code>
      */
     private $user_count = 0;
+    /**
+     *教师总数
+     *
+     * Generated from protobuf field <code>int64 teacher_count = 3;</code>
+     */
+    private $teacher_count = 0;
+    /**
+     *学生总数
+     *
+     * Generated from protobuf field <code>int64 student_count = 4;</code>
+     */
+    private $student_count = 0;
 
     public function __construct() {
         \GPBMetadata\ResourceStatistics::initOnce();
@@ -54,6 +68,8 @@ class ActiveUserItem extends \Google\Protobuf\Internal\Message
     }
 
     /**
+     *总人数
+     *
      * Generated from protobuf field <code>int64 user_count = 2;</code>
      * @return int|string
      */
@@ -63,6 +79,8 @@ class ActiveUserItem extends \Google\Protobuf\Internal\Message
     }
 
     /**
+     *总人数
+     *
      * Generated from protobuf field <code>int64 user_count = 2;</code>
      * @param int|string $var
      * @return $this
@@ -71,6 +89,58 @@ class ActiveUserItem extends \Google\Protobuf\Internal\Message
     {
         GPBUtil::checkInt64($var);
         $this->user_count = $var;
+
+        return $this;
+    }
+
+    /**
+     *教师总数
+     *
+     * Generated from protobuf field <code>int64 teacher_count = 3;</code>
+     * @return int|string
+     */
+    public function getTeacherCount()
+    {
+        return $this->teacher_count;
+    }
+
+    /**
+     *教师总数
+     *
+     * Generated from protobuf field <code>int64 teacher_count = 3;</code>
+     * @param int|string $var
+     * @return $this
+     */
+    public function setTeacherCount($var)
+    {
+        GPBUtil::checkInt64($var);
+        $this->teacher_count = $var;
+
+        return $this;
+    }
+
+    /**
+     *学生总数
+     *
+     * Generated from protobuf field <code>int64 student_count = 4;</code>
+     * @return int|string
+     */
+    public function getStudentCount()
+    {
+        return $this->student_count;
+    }
+
+    /**
+     *学生总数
+     *
+     * Generated from protobuf field <code>int64 student_count = 4;</code>
+     * @param int|string $var
+     * @return $this
+     */
+    public function setStudentCount($var)
+    {
+        GPBUtil::checkInt64($var);
+        $this->student_count = $var;
 
         return $this;
     }
